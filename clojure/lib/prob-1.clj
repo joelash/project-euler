673efc061b2d4eac676303dfa86974fc63817c59
Joel Friedman <jfriedman@lmp-jfriedman01.local>
2010-03-01 23:27:04 -0600

    Revert "using loop with recur"
    This reverts commit 2e2ee0b6c6b8e2bfebca9cc05145246b1353d200.

--- clojure/lib/prob-1.clj
+++ clojure/lib/prob-1.clj
@@ -4,17 +4,8 @@
   (if (or (is-mult? num 3) (is-mult? num 5))
     num
     0))
-(defn prob1 [start end]
-  (loop [total 0 current start end end]
-    (if (= current end)
-      total
-      (recur (+ total (to-add current)) (+ 1 current) end))))
-
-(println (format "Total: %s" (prob1 1 1000)))
-
-
-
-;(defn sum-mults [mult max]
-;  (def x (/ (- max 1) mult))
-;  (/ (* mult x (+ x 1)) 2))
-;(println (format "Total: %s" (- (+ (sum-mults 3 1000) (sum-mults 5 1000)) (sum-mults 15 1000))))
+(defn prob1 [total current end]
+  (if (= current end)
+    total
+    (recur (+ total (to-add current)) (+ 1 current) end)))
+(println (format "Total: %s" (prob1 0 1 1000)))
